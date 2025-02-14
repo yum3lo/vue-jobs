d645bcd44201676bd2b59f69a1cf233c646ec5a4
maria <118808220+yum3lo@users.noreply.github.com>
2025-02-14 14:26:24 +0200

    added the EditJob page

--- src/views/EditJobView.vue
+++ src/views/EditJobView.vue
@@ -1,8 +1,12 @@
 <script setup>
 import router from '@/router';
-import { reactive } from 'vue';
+import { reactive, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 import { useToast } from 'vue-toastification';
 import axios from 'axios';
+
+const route = useRoute();
+const jobId = route.params.id;
 
 const form = reactive({
   type: 'Full-Time',
@@ -18,10 +22,15 @@ const form = reactive({
   }
 });
 
+const state = reactive({
+  job: {},
+  isLoading: true,
+});
+
 const toast = useToast();
 
 const handleSubmit = async () => {
-  const newJob = {
+  const updatedJob = {
     title: form.title,
     description: form.description,
     type: form.type,
@@ -35,14 +44,34 @@ const handleSubmit = async () => {
     }
   };
   try {
-    const response = await axios.post('/api/jobs', newJob);
-    toast.success('Job added successfully');
+    const response = await axios.put(`/api/jobs/${jobId}`, updatedJob);
+    toast.success('Job updated successfully');
     router.push(`/jobs/${response.data.id}`);
   } catch (error) {
     console.error(error);
-    toast.error('Error adding job');
+    toast.error('Error updating job');
   }
 };
+
+onMounted(async () => {
+  try {
+    const response = await axios.get(`/api/jobs/${jobId}`);
+    state.job = response.data;
+    form.type = state.job.type;
+    form.title = state.job.title;
+    form.description = state.job.description;
+    form.salary = state.job.salary;
+    form.location = state.job.location;
+    form.company.name = state.job.company.name;
+    form.company.description = state.job.company.description;
+    form.company.contactEmail = state.job.company.contactEmail;
+    form.company.contactPhone = state.job.company.contactPhone;
+  } catch (error) {
+    console.error('Error fetching data', error);
+  } finally {
+    state.isLoading = false;
+  }
+});
 </script>
 
 <template>
@@ -51,7 +80,7 @@ const handleSubmit = async () => {
       <div class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
         <form @submit.prevent="handleSubmit">
           <h2 class="text-3xl text-center font-semibold mb-6">
-            Add Job
+            Update Job
           </h2>
           <div class="mb-4">
             <label for="type" class="block text-gray-700 font-bold mb-2">
@@ -216,7 +245,7 @@ const handleSubmit = async () => {
               class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline cursor-pointer"
               type="submit"
             >
-              Add Job
+              Update Job
             </button>
           </div>
         </form>
